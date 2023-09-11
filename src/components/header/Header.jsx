20a1507493b0fadfd548c2bddbe45d40840ad371
Ellina Babaian <famogl375@gmail.com>
2023-09-11 13:59:32 -0700

    setting up base layout footer

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 
 const Header = () => {
-    return(
-        <div id="home">
-            <div className="section-header__text">
-                <h1>Front End Developer</h1>
-                <h3>Hi! My name is Ellina Babaian<br></br>A passionate Front End Developer based in Los Angeles, CA</h3>
-            </div>
-            <div className="section-header__photo"></div>
-        </div>
-    )
+  return (
+    <div id="home">
+      <div className="section-header__text">
+        <h1>Front End Developer</h1>
+        <h3>
+          Hi! My name is Ellina Babaian<br></br>A passionate Front End Developer
+          based in Los Angeles, CA
+        </h3>
+      </div>
+      <div className="section-header__photo">
+        <img src="https://avatars.githubusercontent.com/u/128013006?v=4"></img>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
