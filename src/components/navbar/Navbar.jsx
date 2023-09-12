f13fe282b40e2a65af0b1b7de7c9eae52b46e917
Ellina Babaian <famogl375@gmail.com>
2023-09-11 20:31:41 -0700

    Navbar styling

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
@@ -6,12 +7,21 @@ const Navbar = () => {
       <img
         src="https://jobs.traff.ink/wp-content/uploads/2022/09/3527962.png"
         alt="logo"
+        className="navbar_logo"
       />
       <div className="navbar_navigation">
-        <a href="#home">Home</a>
-        <a href="#projects">Projects</a>
-        <a href="#about">About</a>
-        <a href="#contact">Contact</a>
+        <a href="#home" className="navbar_item">
+          Home
+        </a>
+        <a href="#projects" className="navbar_item">
+          Projects
+        </a>
+        <a href="#about" className="navbar_item">
+          About
+        </a>
+        <a href="#contact" className="navbar_item">
+          Contact
+        </a>
       </div>
     </div>
   );
