660f65f4e2bf5341df7673b6c77b3a4ce98e9b55
Ellina Babaian <famogl375@gmail.com>
2023-09-11 21:08:37 -0700

    Styling changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,6 +3,7 @@ import "./Header.css";
 
 const Header = () => {
   return (
+    <div>
     <div className="header__container" id="home">
       <div className="section-header__photo_container">
         <img
@@ -12,17 +13,19 @@ const Header = () => {
         ></img>
       </div>
       <div className="section-header__text">
-        <h1 className="section-header__h1">Front End Developer</h1>
+        <p className="section-header__hi">Hi! My name is</p>
+        <h1 className="section-header__h1">Ellina Babaian</h1>
+        <h3 className="section-header__h3">I build things for the web</h3>
         <h4 className="section-header__h4">
-          Hi! My name is Ellina Babaian<br></br>A passionate Front End Developer
-          based in Los Angeles, CA
+          {/* Hi! My name is Ellina Babaian<br></br> */}
+          I'm a Front End Developer based in Los Angeles, CA
         </h4>
         <div className="section-header__contact">
           <div className="popup__contact_header">
             <a
               href="https://www.linkedin.com/in/ellina-babaian-302645267/"
               target="_blank"
-              className="link_tag"
+              className="link_tag__header"
             >
               <img
                 className="link_logo__header"
@@ -32,7 +35,7 @@ const Header = () => {
             <a
               href="https://github.com/ellinababayan"
               target="_blank"
-              className="link_tag"
+              className="link_tag__header"
             >
               <img
                 className="link_logo__header"
@@ -42,7 +45,7 @@ const Header = () => {
             <a
               href="mailto:famogl375@gmail.com"
               target="_blank"
-              className="link_tag"
+              className="link_tag__header"
             >
               <img
                 className="link_logo__header"
@@ -51,81 +54,70 @@ const Header = () => {
             </a>
           </div>
         </div>
-        <div className="tech_stack__container">
-          <h3 className="section-header__h3">Tech Stack:</h3>
-          <ul className="tech_stack">
-            <li>
-              <img src="img/html-logo.png" alt="HTML" className="stack_logo" />
-            </li>
-            <li>
-              <img src="img/css-logo.png" alt="CSS" className="stack_logo" />
-            </li>
-            <li>
-              <img
-                src="img/js-logo.png"
-                alt="JavaScript"
-                className="stack_logo"
-              />
-            </li>
-            <li>
-              <img
-                src="img/python-logo.png"
-                alt="Python"
-                className="stack_logo"
-              />
-            </li>
-            <li>
-              <img
-                src="img/typescript-logo.png"
-                alt="TypeScript"
-                className="stack_logo"
-              />
-            </li>
-            <li>
-              <img
-                src="img/react-logo.webp"
-                alt="React"
-                className="stack_logo"
-              />
-            </li>
-            <li>
-              <img
-                src="img/redux-logo.png"
-                alt="Redux"
-                className="stack_logo"
-              />
-            </li>
-            <li>
-              <img
-                src="img/node-logo.png"
-                alt="Node.js"
-                className="stack_logo"
-              />
-            </li>
-            <li>
-              <img src="img/sass-logo.png" alt="Sass" className="stack_logo" />
-            </li>
-            <li>
-              <img
-                src="img/bootstrap-logo.png"
-                alt="Bootstrap"
-                className="stack_logo"
-              />
-            </li>
-            <li>
-              <img
-                src="img/jquery-logo.png"
-                alt="jQuery"
-                className="stack_logo"
-              />
-            </li>
-            <li>
-              <img src="img/git-logo.png" alt="Git" className="stack_logo" />
-            </li>
-          </ul>
-        </div>
       </div>
-    </div>
+      
+    </div><div className="tech_stack__container">
+        <h2 className="section-header__h2">Tech Stack:</h2>
+        <ul className="tech_stack">
+          <li>
+            <img src="img/html-logo.png" alt="HTML" className="stack_logo" />
+          </li>
+          <li>
+            <img src="img/css-logo.png" alt="CSS" className="stack_logo" />
+          </li>
+          <li>
+            <img
+              src="img/js-logo.png"
+              alt="JavaScript"
+              className="stack_logo"
+            />
+          </li>
+          <li>
+            <img
+              src="img/python-logo.png"
+              alt="Python"
+              className="stack_logo"
+            />
+          </li>
+          <li>
+            <img
+              src="img/typescript-logo.png"
+              alt="TypeScript"
+              className="stack_logo"
+            />
+          </li>
+          <li>
+            <img src="img/react-logo.webp" alt="React" className="stack_logo" />
+          </li>
+          <li>
+            <img src="img/redux-logo.png" alt="Redux" className="stack_logo" />
+          </li>
+          <li>
+            <img src="img/node-logo.png" alt="Node.js" className="stack_logo" />
+          </li>
+          <li>
+            <img src="img/sass-logo.png" alt="Sass" className="stack_logo" />
+          </li>
+          <li>
+            <img
+              src="img/bootstrap-logo.png"
+              alt="Bootstrap"
+              className="stack_logo"
+            />
+          </li>
+          <li>
+            <img
+              src="img/jquery-logo.png"
+              alt="jQuery"
+              className="stack_logo"
+            />
+          </li>
+          <li>
+            <img src="img/git-logo.png" alt="Git" className="stack_logo" />
+          </li>
+        </ul>
+      </div>
+      </div>
   );
 };
 
