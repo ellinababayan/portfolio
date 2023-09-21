f9a6f0b069ff31e96b04a3062b157ccc33a1d716
Ellina Babaian <famogl375@gmail.com>
2023-09-20 21:04:04 -0700

    projects component refactoring

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import "./Projects.css";
+import { External } from "./Svg";
 
 const Projects = () => {
   return (
     <div className="section-projects" id="projects">
       <h3 className="section-projects__h3">My Projects</h3>
       <div className="project_container">
-        <div className="project_1">
+        <div className="project_1 project__item">
           <a href="https://natours-beta-six.vercel.app/" target="_blank">
-            <video className="project_img" autoPlay="" loop muted>
+            <video className="project_img" autoPlay loop muted>
               <source
-                src="img/Natours _ Exciting tours for adventurous people - Google Chrome 2023-09-11 23-28-31.mp4"
+                // src="img/Natours _ Exciting tours for adventurous people - Google Chrome 2023-09-11 23-28-31.mp4"
+                src="img/Natours_screenrecord.mp4"
                 type="video/mp4"
               />
               Video is not supported by browser
@@ -31,15 +33,16 @@ const Projects = () => {
               target="_blank"
               className="project_link"
             >
-              Visit web site &#8599;
+              Visit web site <External />
             </a>
           </div>
         </div>
-        <div className="project_2">
+        <div className="project_2 project__item">
           <a href="https://trillo-pied-nine.vercel.app/" target="_blank">
-            <video className="project_img" autoPlay="" loop muted>
+            <video className="project_img" autoPlay loop muted>
               <source
-                src="img/Trillo _ Your holiday in one click - Google Chrome 2023-09-11 23-41-29.mp4"
+                // src="img/Trillo _ Your holiday in one click - Google Chrome 2023-09-11 23-41-29.mp4"
+                src="img/Trillo_screenrecord.mp4"
                 type="video/mp4"
               />
               Video is not supported by browser
@@ -60,15 +63,16 @@ const Projects = () => {
               target="_blank"
               className="project_link"
             >
-              Visit web site &#8599;
+              Visit web site <External />
             </a>
           </div>
         </div>
-        <div className="project_3">
+        <div className="project_3 project__item">
           <a href="https://nexter-topaz.vercel.app/" target="_blank">
-            <video className="project_img" autoPlay="" loop muted>
+            <video className="project_img" autoPlay loop muted>
               <source
-                src="img/Nexter — your home, your freedom - Google Chrome 2023-09-11 23-46-57.mp4"
+                // src="img/Nexter — your home, your freedom - Google Chrome 2023-09-11 23-46-57.mp4"
+                src="img/Nexter_screenrecord.mp4"
                 type="video/mp4"
               />
               Video is not supported by browser
@@ -88,7 +92,7 @@ const Projects = () => {
               target="_blank"
               className="project_link"
             >
-              Visit web site &#8599;
+              Visit web site <External />
             </a>
           </div>
         </div>
